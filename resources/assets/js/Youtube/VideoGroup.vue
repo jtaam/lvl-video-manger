405b4af5d1b38c2d28916b6c3e2ab6ff53b91938
Tam Jid <tamjid.mdh@gmail.com>
2018-10-21 11:49:48 +0600

    layouts &  search.js

---
+++ resources/assets/js/Youtube/VideoGroup.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "VideoGroup"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/Youtube/YoutubeDash.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="YoutubeDash__wrapper">
+        YoutubeDash
+    </div>
+</template>
+
+<script>
+    import Search from './Search';
+    export default {
+        created(){
+          Search({
+              apiKey : 'asdf',
+              term: 'laravel repository'
+          });
+        },
+        name: "YoutubeDash"
+    }
+</script>
+
+<style scoped>
+
+</style>
