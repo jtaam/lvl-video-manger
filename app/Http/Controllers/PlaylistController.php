1fb4f2744a3e7157dd6e0e4b1b4609b7f1793775
Tam Jid <tamjid.mdh@gmail.com>
2018-10-27 17:36:49 +0600

    playlist r,m,c

---
+++ app/Http/Controllers/PlaylistController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Playlist;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PlaylistController extends Controller
+{
+    public function index()
+    {
+        $playlist = Playlist::where('user_id', Auth::user()->id)->orderBy('name', 'asc');
+
+        return response()->json($playlist, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'isPrivate' => 'required',
+        ]);
+
+        $playlist = Playlist::create([
+            'name' => $request->input('name'),
+            'user_id' => $request->user()->id,
+            'is_private' => $request->input('isPrivate'),
+        ]);
+
+        return response()->json($playlist, 201);
+    }
+}
